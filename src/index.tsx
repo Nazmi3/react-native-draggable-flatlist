0e15ce8181b404351309a5a7d4dad75139491b1c
Nazmi Hensem <nazmiryuki@gmail.com>
2023-01-04 16:24:24 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import DraggableFlatList from "../src_parent/components/DraggableFlatList";
-export * from "../src_parent/components/CellDecorators";
-export * from "../src_parent/components/NestableDraggableFlatList";
-export * from "../src_parent/components/NestableScrollContainer";
-export * from "../src_parent/types";
+import DraggableFlatList from "../src/components/DraggableFlatList";
+export * from "../src/components/CellDecorators";
+export * from "../src/components/NestableDraggableFlatList";
+export * from "../src/components/NestableScrollContainer";
+export * from "../src/types";
 export default DraggableFlatList;
